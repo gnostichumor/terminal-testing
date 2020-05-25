4144c423e398606d439500f2afa2e35ef22a25e9
gnostichumor <preston.matt86@gmail.com>
2020-05-24 19:18:25 -0700

    Initial Build - no cleanup

---
+++ Assets/Scripts/Input_Window/UI_Input_Window.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CodeMonkey.Utils;
+using TMPro;
+
+public class UI_Input_Window : MonoBehaviour
+{
+
+    private Button_UI okBtn;
+    private Button_UI cancelButton;
+    private TMP_InputField inputText;
+
+    private static UI_Input_Window instance;
+
+    
+    private void Awake()
+    {
+        instance = this;
+
+        if (okBtn == null)
+        {
+            okBtn = transform.Find("SubmitButton").GetComponent<Button_UI>();
+        }
+
+        if (cancelButton == null)
+        {
+            cancelButton = transform.Find("CancelButton").GetComponent<Button_UI>();
+        }
+
+        if (inputText == null)
+        {
+
+            inputText = transform.Find("InputField").GetComponent<TMP_InputField>();
+        }
+        
+        HideInputWindow();
+    }
+
+
+    private void ShowInputWindow(string inputString, Action onCancel, Action<string> onOk)
+    {
+        gameObject.SetActive(true);
+        inputText.text = inputString;
+
+        cancelButton.ClickFunc = () =>
+        {
+            onCancel();
+        };
+
+        okBtn.ClickFunc = () =>
+        {
+            onOk(inputText.text);
+            LogHistory();
+            Debug.Log(inputText.text);
+
+
+        };
+
+
+    }
+    private void HideInputWindow()
+    {
+        gameObject.SetActive(false);
+
+
+    }
+
+
+    public static void Static_ShowInputWindow(string inputString, Action onCancel, Action<string> onOk)
+    {
+        instance.ShowInputWindow(inputString, onCancel, onOk);
+
+    }
+
+    public static void Static_HideInputWindow()
+    {
+        instance.HideInputWindow();
+    }
+
+    public void LogHistory()
+    {
+        inputText.text = null;
+    }
+
+}
